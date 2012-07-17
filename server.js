e3ca467007382687d267a3d040113699f498a326
Lauren Caswell <lauren.puppy@gmail.com>
2012-07-17 10:22:44 -0500

    Implimented Login

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
-var app = require('express').createServer();
+var express = require('express');
+var app = express.createServer();
+
+app.use(express.logger());
+app.use(express.bodyParser());
+
+app.use(express.cookieParser());
+app.use(express.session({secret: "lamp post"}));
 
 app.get('/', function(req, res) {
-  console.log(req.headers);
-  res.end("GET /");
+  if (!req.session.username) {
+    res.redirect("/login");
+    return;
+  }
+  console.log(req.session);
+  res.render("home.ejs", {username:req.session.username});
 });
  
 app.get('/login', function(req, res) {
-  res.end("GET /login");
+  res.render("login.ejs")
 });
 
 app.post('/login', function(req, res) {
-  res.end("POST /login");
+  console.log(req.body , req.body.username , users[req.body.username] , req.body.password);
+  if (users[req.body.username] === req.body.password) {
+    req.session.username = req.body.username;
+    res.redirect("/");
+    return;
+  }
+  res.redirect("/login");
 });
 
 app.post('/logout', function(req, res) {
-  res.end("POST /logout");
+  req.session.username = false;
+  res.redirect("/login");
 });
 
 app.listen(8080);
+
+var users= {
+  "username": "password"
+};
