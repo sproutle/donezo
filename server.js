9e27f02cbcd5a9169e0cfcbf34fead3833a0f508
Lauren Caswell <lauren.puppy@gmail.com>
2012-07-17 16:03:56 -0500

    Just some comments

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+//use express now, create server//
 var express = require('express');
 var app = express.createServer();
 
+//checks login//
 app.use(express.logger());
 app.use(express.bodyParser());
 
+//cookie and session while logged in//
 app.use(express.cookieParser());
 app.use(express.session({secret: "lamp_post"}));
 
-app.use(express.stactic(__dirname + '/public'));
-
+//GET function, home page//
 app.get('/', function(req, res) {
   if (!req.session.username) {
     res.redirect("/login");
@@ -18,10 +20,12 @@ app.get('/', function(req, res) {
   res.render("home.ejs", {username:req.session.username});
 });
  
+ //GET login function, login page//
 app.get('/login', function(req, res) {
   res.render("login.ejs")
 });
 
+//POST login function, logs in//
 app.post('/login', function(req, res) {
   if (users[req.body.username] === req.body.password) {
     req.session.username = req.body.username;
@@ -31,13 +35,18 @@ app.post('/login', function(req, res) {
   res.redirect("/login");
 });
 
+//POST logout function, logs out//
 app.post('/logout', function(req, res) {
   req.session.username = false;
   res.redirect("/login");
 });
 
-app.listen(8080);
+//can now use public folder//
+app.use(express.static(__dirname + '/public'));
 
+//example database for users/passwords//
 var users= {
   "username": "password"
 };
+
+app.listen(8080);
