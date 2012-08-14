7fd0b456cd2a57ffa0ef6e3408ee06200057229a
Lauren Caswell <lauren.puppy@gmail.com>
2012-08-14 10:44:28 -0500

    Update dependencies and add test folder

---
+++ test/test.js
@@ -0,0 +1,11 @@
+describe("my test", function () {
+  it("should work", function (done) {
+    done();
+  });
+  it("should work more", function (done) {
+    done();
+  });
+  it("should work all the time", function (done) {
+    done();
+  });
+});
